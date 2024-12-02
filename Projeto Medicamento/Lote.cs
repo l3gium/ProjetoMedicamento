ebd872fb0444e4a3347fdc5e6bb1715a9c6c19e8
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-12-02 00:28:49 -0300

    Add files via upload

---
+++ Projeto Medicamento/Lote.cs
@@ -0,0 +1,59 @@
+using System.Runtime.CompilerServices;
+
+namespace Projeto_Medicamento;
+
+public class Lote
+{
+    private int id, qtde;
+    private DateTime venc;
+    
+    public Lote()
+    {
+        this.id = 0;
+        this.qtde = 0;
+        this.venc = DateTime.Now;
+    }
+
+    public Lote(int id, int qtde, DateTime venc)
+    {
+        this.id = id;
+        this.qtde = qtde;
+        this.venc = venc;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getQtde()
+    {
+        return this.qtde;
+    }
+
+    public void setQtde(int qtde)
+    {
+        this.qtde = qtde;
+    }
+
+    public DateTime getVenc()
+    {
+        return this.venc;
+    }
+
+    public void setVenc(DateTime venc)
+    {
+        this.venc = venc;
+    }
+
+    public override string ToString()
+    {
+        return this.id.ToString() + " - " + this.qtde.ToString() + " - " + this.venc.ToString();
+    }
+}
+
---
+++ Projeto Medicamento/Medicamentos.cs
@@ -0,0 +1,36 @@
+namespace Projeto_Medicamento;
+
+public class Medicamentos
+{
+    private List<Medicamento> listaMedicamentos = new List<Medicamento>();
+
+    public Medicamentos()
+    {
+        this.listaMedicamentos = new List<Medicamento>();
+    }
+
+    public List<Medicamento> getListaMedicamentos()
+    {
+        return this.listaMedicamentos;
+    }
+    
+    public void Adicionar(Medicamento medicamento)
+    {
+        listaMedicamentos.Add(medicamento);
+    }
+
+    public bool Deletar(Medicamento medicamento)
+    {
+        if (medicamento.QtdeDisponivel() == 0)
+        {
+            return listaMedicamentos.Remove(medicamento);
+        }
+
+        return false;
+    }
+
+    public Medicamento Pesquisar(Medicamento medicamento)
+    {
+        return listaMedicamentos.FirstOrDefault(m => m.Equals(medicamento)) ?? new Medicamento();
+    }
+}
